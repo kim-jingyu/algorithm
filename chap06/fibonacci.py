8b1e98e5dd11f1ae1328991f575c6617826db517
JingyuKim <swc05161@gmail.com>
2023-10-18 11:48:32 +0900

    feat: 피보나치 수열

---
+++ chap06/fibonacci.py
@@ -0,0 +1,29 @@
+# 1 1 2 3 5 8 13
+
+def fibonacci_loop(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [1]
+    elif n == 2:
+        return [1, 1]
+
+    fibo_arr = [1, 1]
+    for i in range(2, n):
+        fibo_arr.append(fibo_arr[i - 1] + fibo_arr[i - 2])
+    return fibo_arr
+
+
+def fibonacci_recursive(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [1]
+    elif n == 2:
+        return [1, 1]
+
+    return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+
+
+if __name__ == '__main__':
+    print(fibonacci_loop(10))
