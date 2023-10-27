0a786bbf1625746a64e788ccefe6be8bbced06e0
JingyuKim <swc05161@gmail.com>
2023-10-27 16:50:44 +0900

    feat: LCS

---
+++ longest_common_subsequence.py
@@ -0,0 +1,17 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+string_A = input()
+string_B = input()
+
+LCS = [[] * len(string_A) for _ in range(len(string_B))]
+
+for i in range(len(string_A)):
+    for j in range(len(string_B)):
+        if i == 0 and j == 0:
+            LCS[i][j] = 0
+        elif string_A[i] == string_B[j]:
+            LCS[i][j] = LCS[i - 1][j - 1] + 1
+        else:
+            LCS[i][j] = max(LCS[i - 1][j], LCS[i][j - 1])
