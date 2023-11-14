bfe5c245a69b8b4b29a284a92b82d0f672ebb863
kim-jingyu <swc05161@gmail.com>
2023-11-14 13:37:57 +0900

    feat: 힙 소트

--- heap_sort.py
+++ heap_sort.py
@@ -1,37 +1,31 @@
-def heap_sort(a):
-    def down_heap(a, left, right):
-        temp = a[left]
-
-        parent = left
-        while parent < (right + 1) // 2:
-            cl = parent * 2 + 1
-            cr = cl + 1
-            child = cr if cr <= right and a[cr] else cl
-            if temp >= a[child]:
-                break
-            a[parent] = a[child]
-            parent = child
-        a[parent] = temp
-
-    n = len(a)
-
-    for i in range((n - 1) // 2, -1, -1):
-        down_heap(a, i, n - 1)
-
-    for i in range(n - 1, 0, -1):
-        a[0], a[i] = a[i], a[0]
-        down_heap(a, 0, i - 1)
+num = 9
+heap = [7, 6, 5, 8, 3, 5, 9, 1, 6]
 
 
-if __name__ == '__main__':
-    print('힙 정렬 수행')
-    num = int(input('원소 수를 입력하세요: '))
-    x = [None] * num
+def down_heap(arr, left, right):
+    root = arr[left]
+    parent = left
 
-    for i in range(num):
-        x[i] = int(input(f'x[{i}]: '))
+    while parent < (right + 1) // 2:
+        child_left = parent * 2 + 1
+        child_right = child_left + 1
+        child = child_right if child_right <= right and arr[child_right] > arr[child_left] else child_left
 
-    heap_sort(x)
+        if root >= arr[child]:
+            break
 
-    for i in range(num):
-        print(f'x[{i}] = {x[i]}')
+        arr[parent] = arr[child]
+        parent = child
+
+    arr[parent] = root
+
+
+for i in range((num - 1) // 2, -1, -1):
+    down_heap(heap, i, num - 1)
+
+
+for i in range(num - 1, 0, -1):
+    heap[0], heap[i] = heap[i], heap[0],
+    down_heap(heap, 0, i - 1)
+
+print(*heap)
